01a0d0488c97012b1457d368c3bec91bb1a5713b
Dmitriy Grid <35104580+qcp@users.noreply.github.com>
2020-07-12 18:36:36 +0300

    some fix

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
       <v-container fill-height>
         <v-layout wrap align-center>
           <v-flex>
+            <v-dialog :value="!loaded" hide-overlay persistent>
+              <v-overlay :value="true">
+                <v-card color="primary" dark width="400">
+                  <v-card-text>
+                    Waiting to establish connection...
+                    <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+                  </v-card-text>
+                </v-card>
+              </v-overlay>
+            </v-dialog>
             <router-view />
           </v-flex>
         </v-layout>
@@ -14,18 +24,25 @@
 
 <script>
 import { USER_REQUEST } from "@/store/actions/user";
+import apiCall from "@/utils/api";
 
 export default {
   name: "App",
-  created: function() {
-    if (this.$store.getters.isAuthenticated) {
-      this.$store.dispatch(USER_REQUEST);
-    }
-  },
-  components: {},
-
   data: () => ({
+    loaded: false,
     dialog: false
-  })
+  }),
+  created: function() {
+    apiCall({ url: "" })
+      .then(() => {
+        this.loaded = true;
+        if (this.$store.getters.isAuthenticated) {
+          this.$store.dispatch(USER_REQUEST);
+        }
+      })
+      .catch(() => {
+
+      });
+  }
 };
 </script>
